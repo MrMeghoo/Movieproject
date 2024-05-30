7defd145ebc62dcc116030ddc59d6d8db554681b
MrMeghoo <mikelmeghoo@gmail.com>
2024-05-30 09:55:07 -0400

    deploy

---
+++ Classwork/dice.js
@@ -0,0 +1,39 @@
+//Using javascript's random number generator, create function that replicates two dice rolls 
+//with possible values between 1-6 for each die. 
+//Two users will each get one throw per turn. Whoever rolls the higher number wins. 
+//For example, if user 1 rolls an 11 and user 2 rolls a 9 then print user 1 wins.  
+//The function should accept each die value as a parameter so the function should be diceRoll(die1, die2
+
+
+
+    
+    // Player name 
+   
+   
+    let Player1roll1= Math.floor(Math.random() * 6) + 1;
+    console.log(Player1roll1);
+    let Player1roll2 = Math.floor(Math.random() * 6) + 1;
+    console.log(Player1roll2)
+    
+    //user 2 Rolls
+    let Player2roll1= Math.floor(Math.random() * 6) + 1;
+    console.log(Player2roll1)
+    let Player2roll2 = Math.floor(Math.random() * 6) + 1;
+    console.log(Player2roll2)
+
+
+
+
+
+    
+
+
+
+    
+    
+    //Make the rollDice function roll dice
+ 
+    
+
+  
+
---
+++ Classwork/myfirst.js
@@ -0,0 +1,49 @@
+//
+
+
+const topcities= ["Tokyo", "Delhi", "Shanghai", "Dhaka", "Sao Paulo", "Cairo", "Mexico City"];
+
+
+let Tokyo={cityName:"Tokyo", 
+ countryName:"Japan", 
+ population:"37115035",
+ yearfounded:"660"}
+
+ let Delhi={cityName:"Delhi", 
+ countryName:"India",
+ population:"33807403",
+ yearfounded:"1052"
+}
+
+ let Shanghai={
+ countryName:"China",
+ population:"29867918",
+ yearfounded:"1291"
+ }
+
+ let Dhaka={
+countryName:"Bangladesh",
+population:"23935652",
+yearfounded:"1608"
+ }
+
+ let SaoPaulo={
+countryName:"Brasil",
+population:"22806704",
+yearfounded:"1554"
+ }
+
+ let Cairo={
+countryName:"Egypt",
+population:"22623874",
+yearfounded:"969"
+ }
+
+ let MexicoCity={
+countryName:"Mexico",
+population:"22505315",
+yearfounded:"1325"
+ }
+
+ console.log("myfirst, java!")
+ my
